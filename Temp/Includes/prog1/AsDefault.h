8e0fdbe84ba2ab02116ca139763dab0c496351e7
alexaandersokolov <72751383+alexaandersokolov@users.noreply.github.com>
2020-10-12 18:47:04 +0300

    laba1
    laba1

--- Temp/Includes/prog1/AsDefault.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PROG1_DEFAULT_318259068
-#define _PROG1_DEFAULT_318259068
-#include "../AsDefault.h"
-#include <bur/plctypes.h>
-#include <bur/plc.h>
-#include <typesTYP.h>
-#include <variablesVAR.h>
-#endif
